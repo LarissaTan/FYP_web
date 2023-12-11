aadffa1caca9ba8f7856df4c0c746a60258f0e14
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-12-11 16:51:32 +0800

    web is connected to smart contract

--- IoT_website/src/main/java/getData.java
+++ IoT_website/src/main/java/getData.java
@@ -1,34 +1,46 @@
-// YourJavaFile.java
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Function;
+import org.mozilla.javascript.Script;
+import org.mozilla.javascript.Scriptable;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.charset.StandardCharsets;
 
 public class getData {
     public static void main(String[] args) {
-        // 创建 JavaScript 引擎
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("javascript");
+        // 创建 Rhino 上下文
+        Context cx = Context.enter();
+        Scriptable scope = cx.initStandardObjects();
 
         // 读取 JavaScript 文件内容
         try {
             String scriptPath = "/Users/tanqianqian/Desktop/FYP_web/IoT_website/src/smartContract.js";
             String scriptContent = new String(Files.readAllBytes(Paths.get(scriptPath)), StandardCharsets.UTF_8);
 
-            //String scriptContent = new String(Files.readAllBytes(Paths.get("smartContract.js")), StandardCharsets.UTF_8);
+            // 编译 JavaScript 代码
+            Script script = cx.compileString(scriptContent, "script", 1, null);
 
             // 执行 JavaScript 代码
-            engine.eval(scriptContent);
+            script.exec(cx, scope);
 
-            // 调用 JavaScript 函数
-            Object result = engine.eval("getAllMessages()");
-            System.out.println("All Messages: " + result);
-        } catch (ScriptException | IOException e) {
+            // 获取 JavaScript 函数对象
+            Object obj = scope.get("getAllMessages", scope);
+            if (obj instanceof Function) {
+                Function getAllMessages = (Function) obj;
+
+                // 调用 JavaScript 函数
+                Object result = getAllMessages.call(cx, scope, scope, new Object[]{});
+                System.out.println("All Messages: " + Context.jsToJava(result, Object.class));
+            } else {
+                System.out.println("Function getAllMessages not found");
+            }
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            // 释放上下文
+            Context.exit();
         }
     }
 }
--- IoT_website/src/smartContract.js
+++ IoT_website/src/smartContract.js
@@ -1,17 +1,96 @@
+
+// Rhino 导入
+//var Web3 = Java.type('org.web3j.protocol.Web3j');
+//var System = Java.type('java.lang.System');
+
 // Connect to the Ethereum node
-var Web3 = Java.type('org.web3j.protocol.Web3j');
 var web3;
 if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
 } else {
     // set the provider you want from Web3.providers
-    web3 = new Web3(new Web3.providers.HttpProvider("https://goerli.infura.io/v3/8600c337513c4cbc8e8d3d9f1f577598"));
+    web3 = new Web3("https://goerli.infura.io/v3/8600c337513c4cbc8e8d3d9f1f577598");
 }
 
 // Contract address and ABI
 var contractAddress = '0x408449Ae12bF1f3154b422f56f9BEBB04B6B1b9d';
 var contractABI = [
-    // Contract ABI (same as the one used in the Python code)
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "newMessage",
+                "type": "string"
+            }
+        ],
+        "name": "addMessage",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getAllMessages",
+        "outputs": [
+            {
+                "internalType": "string[]",
+                "name": "",
+                "type": "string[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getLastMessage",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "index",
+                "type": "uint256"
+            }
+        ],
+        "name": "getMessage",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "index",
+                "type": "uint256"
+            },
+            {
+                "internalType": "string",
+                "name": "newMessage",
+                "type": "string"
+            }
+        ],
+        "name": "setMessage",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }// Contract ABI (same as the one used in the Python code)
 ];
 
 // Create contract instance
@@ -22,10 +101,13 @@ function getAllMessages() {
     return contract.methods.getAllMessages().call();
 }
 
-// Call the function and log the result
-try {
-    var messages = getAllMessages();
-    print('All Messages:', JSON.stringify(messages));
-} catch (error) {
-    print('Error:', error);
-}
+
+window.onload = function() {
+    try {
+        var messages = getAllMessages();
+        document.getElementById('out1').innerText = "All Messages: " + JSON.stringify(messages);
+    } catch (error) {
+        document.getElementById('out1').innerText = "Error: " + error;
+    }
+};
+
